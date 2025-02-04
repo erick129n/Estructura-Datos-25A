0ea34053ddb73534a6a803be221c0e1049b27d36
Erick <gonzalezcarrilloerickmanuel29@gmail.com>
2025-02-04 12:12:17 -0600

    la serie

---
+++ C++/la serie/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+double funcion(double x);
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+
+double funcion(double x){
+    x *= x;
+    x+2;
+    return x;
+}
